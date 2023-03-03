85438780e1c23a7f935c4d45e4b16038354e5b33
hearanchoi <122843227+hearanchoi@users.noreply.github.com>
2023-03-03 22:35:04 +0900

    [Bronze V] Title: 나머지, Time: 208 ms, Memory: 17744 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
 
-public class Main {
-
+public class Main{
+	
 	public static void main(String[] args) {
 		
-	Scanner sc = new Scanner(System.in);
-	
-	int A = sc.nextInt();
-	int B = sc.nextInt();
-	int C = sc.nextInt();
-	
-	System.out.println((A+B)%C);
-	System.out.println(((A%C)+(B%C))%C);
-	System.out.println((A*B)%C);
-	System.out.println(((A%C)*(B%C))%C);
-	
+		Scanner sc = new Scanner(System.in);
+		
+		int A, B, C; 
+		
+		 A = sc.nextInt();
+		 B = sc.nextInt();
+		 C = sc.nextInt();
+		
+		System.out.println((A+B)%C);
+		System.out.println(((A%C) + (B%C))%C);
+		System.out.println((A*B)%C);
+		System.out.println(((A%C)*(B%C))%C);
+		
 	}
 }
